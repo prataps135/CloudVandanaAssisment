ca6b0375fe0771836cbc74b2e7b6ed41b91ccb3d
Pratap Singh Sisodia <48864550+prataps135@users.noreply.github.com>
2023-10-11 22:41:08 +0530

    Functionality completed (keyup is also added).

--- HTML/Calculator/index.js
+++ HTML/Calculator/index.js
@@ -79,3 +79,93 @@ function cleaning() {
   isSignAdded = false;
   operation = null;
 }
+
+// adding key press functionality
+document.addEventListener("keyup", (event) => {
+  console.log(event.key);
+  switch (event.key) {
+    case "1":
+      addValue(1);
+      break;
+    case "2":
+      addValue(2);
+      break;
+    case "3":
+      addValue(3);
+      break;
+    case "4":
+      addValue(4);
+      break;
+    case "5":
+      addValue(5);
+      break;
+    case "6":
+      addValue(6);
+      break;
+    case "7":
+      addValue(7);
+      break;
+    case "8":
+      addValue(8);
+      break;
+    case "9":
+      addValue(9);
+      break;
+    case "0":
+      addValue(0);
+      break;
+    case "+":
+      isSignAdded = true;
+      operation = "+";
+      break;
+    case "-":
+      isSignAdded = true;
+      operation = "-";
+      break;
+    case "/":
+      isSignAdded = true;
+      operation = "%";
+      break;
+    case "*":
+      isSignAdded = true;
+      operation = "x";
+      break;
+    case ".":
+      addValue(".");
+      break;
+    case "Enter":
+      let tempValue1 = parseInt(value1.join(""));
+      let tempValue2 = parseInt(value2.join(""));
+
+      switch (operation) {
+        case "+":
+          display.innerHTML = add(tempValue1, tempValue2);
+          break;
+        case "-":
+          display.innerHTML = sub(tempValue1, tempValue2);
+          break;
+        case "x":
+          display.innerHTML = multiply(tempValue1, tempValue2);
+          break;
+        case "%":
+          display.innerHTML = divide(tempValue1, tempValue2);
+          break;
+      }
+      cleaning();
+      break;
+    case "Escape":
+      display.innerHTML = 0;
+      cleaning();
+      break;
+  }
+});
+
+function addValue(value) {
+  if (!isSignAdded) {
+    value1.push(value);
+    display.innerHTML = value1.join("");
+  } else {
+    value2.push(value);
+    display.innerHTML = value2.join("");
+  }
+}
