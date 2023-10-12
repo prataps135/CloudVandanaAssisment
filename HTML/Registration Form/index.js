063988d794a0b2f4369910215118aa7bb3b1d5fe
Pratap Singh Sisodia <48864550+prataps135@users.noreply.github.com>
2023-10-12 11:52:53 +0530

    form completed

---
+++ HTML/Registration Form/index.js
@@ -0,0 +1,8 @@
+let regForm = document.getElementById("registrationForm")
+
+
+// submitting the form
+let submitBtn = document.getElementById("submit");
+submitBtn.addEventListener("click",()=>{
+})
+console.log(regForm);
